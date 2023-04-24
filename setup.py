03b59fdf516badbd5cae868f80fd4f26f0a641d8
EinKara <ironmasterz@outlook.com>
2023-04-24 12:28:28 -0400

    please

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="PyCHAS",
     version="0.0.1",
+    description="Python C/C++ header amalgamation script",
     entry_points={
         'console_scripts':[
             'chas=PyCHAS.chas:entry'
